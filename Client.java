4383ff324248fdebebde574bb072270b867aa8ab
Ian Nguyen S3788210 <s3788210@student.rmit.edu.au>
2020-11-26 16:16:16 +1100

    Initial

---
+++ Client.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.net.*;
+
+/**
+ *
+ * @author darren
+ */
+public class Client {
+
+    public static void main(String arg[]) throws IOException, ClassNotFoundException {
+
+        int portNum = 11113;
+
+        Socket socket = new Socket("54.82.80.62", portNum);
+
+        // Integer Object to send to Server.
+        Integer num = 50;
+
+        ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+        ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+        
+        out.writeObject(num);
+                
+        String response = (String) in.readObject();
+
+        System.out.println("Server message: " + response);
+        
+        //socket.close();
+        
+    }
+}
